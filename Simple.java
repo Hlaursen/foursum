4bc23e77498e9c2bfadd945eabd6526fc87b176f
Hlaursen <35952936+Hlaursen@users.noreply.github.com>
2018-02-23 10:43:11 +0100

    Simple.java with timing inserted

--- Simple.java
+++ Simple.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
+import edu.princeton.cs.algs4.*;
 
-public class Simple {
-    public static void main(String[] args) {
-	       Scanner S = new Scanner(System.in);
+public class Simple
+{
+    public static void main(String[] args)
+    {
+        Stopwatch timer = new Stopwatch();
+        double start = timer.elapsedTime();
+         Scanner S = new Scanner(System.in);
 	       int N = Integer.parseInt(S.nextLine());
 	       long[] vals = new long[N];
 	       for(int i = 0; i < N; i+= 1) vals[i] = Long.parseLong(S.nextLine());
@@ -14,13 +19,23 @@ public class Simple {
 
                  if (vals[i] + vals[j] + vals[k] + vals[l] == 0) {
                       System.err.println(i+" "+j+" "+k+" "+l);
+
+                      //Timing
+                      System.out.println(N);
+                      double end = timer.elapsedTime();
+                      System.out.println(end-start);
                       System.out.println(true);
+
                       System.exit(0);
                  }
                }
              }
            }
          }
+         //Timing
+         System.out.println(N);
+         double end = timer.elapsedTime();
+         System.out.println(end-start);
 
          System.out.println(false);
     }
